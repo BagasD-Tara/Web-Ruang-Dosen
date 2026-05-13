0d54bf19e85e47ccf0b52299475ab532f6881c41
Vanzaaa04 <arielardiansyah050316@gmail.com>
2026-05-13 23:33:02 +0700

    feat: tambah endpoint mata kuliah baru (handled by Aril)

--- apps/api/src/course/course.controller.ts
+++ apps/api/src/course/course.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CourseService } from './course.service';
+import { Course } from '@prisma/client';
 
-@Controller('course')
-export class CourseController {}
+@Controller('courses')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Post()
+  async create(
+    @Body() data: { title: string; description?: string; instructorId: string }
+  ): Promise<Course> {
+    return this.courseService.create(data);
+  }
+}
