b54ca13afa59946bf268ccbea78af07fe4758b77
Vanzaaa04 <arielardiansyah050316@gmail.com>
2026-05-30 18:52:24 +0700

    feat: penambahan pengumpulan tugas dan grading dosen sprint 3 (handled by ariel)

--- apps/api/src/assignment/assignment.module.ts
+++ apps/api/src/assignment/assignment.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AssignmentController } from './assignment.controller';
 import { AssignmentService } from './assignment.service';
 import { PrismaService } from '../prisma.service';
+import { AssignmentSubmissionController } from './assignment-submission.controller';
 
 @Module({
-  controllers: [AssignmentController],
-  providers: [AssignmentService, PrismaService]
+  controllers: [AssignmentController, AssignmentSubmissionController],
+  providers: [AssignmentService, PrismaService],
 })
 export class AssignmentModule {}
